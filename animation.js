6af12245f4c3056e2229a939637401e978cf88e3
Yuqiwu <ywu@stuy.edu>
2018-03-01 21:31:39 -0500

    finished

--- animation.js
+++ animation.js
@@ -1,5 +1,4 @@
 var box = document.getElementById("box");
-var ctx = box.getContext("2d");
 var startit = document.getElementById("start");
 var stopit = document.getElementById("stop");
 var toggleit = document.getElementById("toggle");
@@ -7,62 +6,61 @@ var toggleit = document.getElementById("toggle");
 // switch between mode
 // 1 for grow and shrink
 // 2 for dvd movement
-var tog = 1; 
+var tog = 1;
+
+// timer ID
+var ID = 0;
+
+// animation status
+var start = true;
 
 // grow and shrink properties
-var ID1;
 var stat = true;
 var r = 0;
 
 // dvd movement properties
-var ID2;
 var x = 100;
 var y = 200;
-var vx = 8;
-var vy = 13;
+var vx = 6;
+var vy = 9;
 
 var stop = function(){
-    if (tog == 1){
-	clearInterval(ID1);
-    }
-    else{
-	clearInterval(ID2);
-    }
+    start = false;
+    clearInterval(ID);
 }
 
 var toggle = function(){
     stop();
     if (tog == 1){
 	tog = 2;
+	dvd();
     }
     else{
 	tog = 1;
+	grow();
     }
-    draw();
 }
 
 var clear = function(){
     box.innerHTML = null;
 }
 
-var draw = function(){
-    stop();
-    var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    circle.setAttribute("fill", "red");
-    var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-    rect.setAttribute("fill", "red");
-    box.appendChild(circle);
-    box.appendChild(rect);
+var grow = function(){
+    clear();
     
-    var animate1 = function(){	
-	circle.setAttribute("cx", "250");
-	circle.setAttribute("cy", "250");
-	circle.setAttribute("r", toString(r) );
+    var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.setAttribute("cx", "250");
+    circle.setAttribute("cy", "250");
+    circle.setAttribute("r", r.toString() );
+    box.appendChild(circle);
+    
+    var animate = function(){
+	circle.setAttribute("r", r.toString() );
+	circle.setAttribute("fill", "red");
 	status();
-	ID1 = setInterval(animate1, 100);
 	console.log("drawing circle");
     }
-
+    
     var status = function(){
 	if (stat == true){
 	    r++;
@@ -77,37 +75,54 @@ var draw = function(){
 	    }
 	}
     }
+    
+    ID = setInterval(animate, 10);
+}
 
-    var animate2 = function(){	
-	rect.setAttribute("x", toString(x) );
-	rect.setAttribute("y", toString(y) );
-	rect.setAttribute("width", "20" );
-	rect.setAttribute("height", "20" );	
+var dvd = function(){
+    clear();
+
+    var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    rect.setAttribute("fill", "red");
+    rect.setAttribute("width", "30" );
+    rect.setAttribute("height", "20" );
+    rect.setAttribute("x", x.toString() );
+    rect.setAttribute("y", y.toString() );
+    box.appendChild(rect);
+    
+    var animate = function(){
+	rect.setAttribute("x", x.toString() );
+	rect.setAttribute("y", y.toString() );
 	movement();
-	ID2 = setInterval(animate2);
     }
 
     var movement = function(){
-	if (x <= 0 || x >= 500){
+	if (x <= 15 || x >= 485){
 	    vx = -vx;
 	}
-	if (y <= 0 || y >= 500){
+	if (y <= 10 || y >= 490){
 	    vy = -vy;
 	}
 	x = x + vx;
 	y = y + vy;
     }
+    ID = setInterval(animate,10);
+}
 
-    if (tog == 1){
-	animate1();
-    }
-    else{
-	animate2();
+var start = function(){
+    if (start == false){
+	if (tog == 1){
+	    grow();
+	}
+	else{
+	    dvd();
+	}
+	start = true;
     }
 }
 
 
-draw();
+grow();
 
 
 startit.addEventListener("click", start);
